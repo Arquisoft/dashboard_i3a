c580defb4dd8fc051a074a53f6b84ede2214e510
dgsama <dgsama141@gmail.com>
2017-04-03 01:18:36 +0200

    Tests added

--- src/test/java/es/uniovi/asw/AllTests.java
+++ src/test/java/es/uniovi/asw/AllTests.java
@@ -15,12 +15,16 @@ import es.uniovi.asw.dto.UserLoginTest;
 import es.uniovi.asw.logicinfo.VotesInfoGeneroTest;
 import es.uniovi.asw.logicinfo.VotesInfoRangoEdadTest;
 import es.uniovi.asw.logicinfo.VotesInformationTest;
+import es.uniovi.asw.repository.CommentRepositoryTest;
+import es.uniovi.asw.repository.ProposalRepositoryTest;
+import es.uniovi.asw.repository.UserRepositoryTest;
 import es.uniovi.asw.util.AgeCalculatorTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({ UserTest.class, ProposalTest.class, VoteCommentTest.class, VoteProposalTest.class, CommentTest.class,
-		AgeCalculatorTest.class, UserLoginTest.class, UserDtoTest.class, ProposalDtoTest.class,VotesInfoGeneroTest.class,
-		VotesInfoRangoEdadTest.class, VotesInformationTest.class})
+		AgeCalculatorTest.class, UserLoginTest.class, UserDtoTest.class, ProposalDtoTest.class,
+		VotesInfoGeneroTest.class, VotesInfoRangoEdadTest.class, VotesInformationTest.class,
+		CommentRepositoryTest.class, ProposalRepositoryTest.class, UserRepositoryTest.class })
 public class AllTests {
 
 }
